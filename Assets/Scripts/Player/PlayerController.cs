83f6d381cebc7dfba31d83fae5e3dd9fb84d3f2d
SuccubHunter <kirilloriginallolo@gmail.com>
2020-06-19 15:29:44 +0500

    Add Player and Controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,104 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Пихаем всю логику управления персонажем в один класс. Да за такое ломают руки, но всё же почему бы и нет?
 public class PlayerController : MonoBehaviour
 {
+	[SerializeField] private float MaxSpeed;
+	public float Speed;
+	public int CountJump;
+	public float PowerJump;
+	[SerializeField] private int MaxJump;
+
+	[SerializeField] private bool isGrounded;
+	[SerializeField] private bool FaceLeft = false;
+
+	[Header("Зависимости")]
+	[SerializeField] private SpriteRenderer Sprite;
+	[SerializeField] private Rigidbody2D RigidBody;
+	[SerializeField] private BoxCollider2D Box;
+	[SerializeField] private CircleCollider2D Circle;
+	[Header("Костыли")]
+	[SerializeField] private List<Collider2D> GroundColliders;
 	void Start()
 	{
-		
+		Circle = GetComponent<CircleCollider2D>();
+		Box = GetComponent<BoxCollider2D>();
+		RigidBody = GetComponent<Rigidbody2D>();
+		Sprite = GetComponent<SpriteRenderer>();
 	}
 
 	void Update()
 	{
+		if (GroundColliders.Count > 0)
+		{
+			isGrounded = true;
+		}
+		else
+		{
+			isGrounded = false;
+		}
+			
+		if (Input.GetButton("Horizontal"))
+		{
+			Move();
+		}
+
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			Sprite.flipX = true;
+			FaceLeft = true;
+		}
+		else if (Input.GetKey(KeyCode.D) && FaceLeft == true)
+		{
+			Sprite.flipX = false;
+			FaceLeft = false;
+		}
+
+		if (Input.GetKeyDown("space"))
+		{
+			Jump();
+		}
+
+		if (isGrounded)
+		{
+			CountJump = MaxJump;
+		}
+	}
+
+	void FixedUpdate()
+	{
 		
+	}
+
+	void OnCollisionStay2D(Collision2D coll)
+	{
+		if (!GroundColliders.Contains(coll.collider))
+			foreach (var p in coll.contacts)
+				if (p.point.y < Box.bounds.min.y)
+				{
+					GroundColliders.Add(coll.collider);
+					break;
+				}
+	}
+
+	void OnCollisionExit2D(Collision2D coll)
+    {
+        if (GroundColliders.Contains(coll.collider))
+            GroundColliders.Remove(coll.collider);
+    }
+	void Move()
+	{
+		Vector3 tempvector = Vector3.right * Input.GetAxis("Horizontal");
+		transform.position = Vector3.MoveTowards(transform.position, transform.position + tempvector, Speed * Time.deltaTime);
+	}
+
+	void Jump()
+	{
+		if (CountJump > 0)
+		{
+			CountJump--;
+			RigidBody.velocity = Vector2.up * PowerJump;
+		}
 	}
 }
